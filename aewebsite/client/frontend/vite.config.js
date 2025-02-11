7c6c0a16a23627db24484a6b72421b74683d6cc1
GSWForever8 <aidanhong8@icloud.com>
2025-02-11 12:29:05 -0800

    please work

--- aewebsite/client/frontend/vite.config.js
+++ aewebsite/client/frontend/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.JPEG'],
+  build:{
+    target: 'esnext',
+  },
 })
